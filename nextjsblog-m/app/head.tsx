dbc579946cc67b5a1398679bde0d63a14ef3b1f1
guleao <guztavusz@gmail.com>
2024-06-07 11:25:40 -0300

    Configurações do blog

---
+++ nextjsblog-m/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+    return (
+        <>
+            <title>Augusto Leao Blog</title>
+            <meta content="width-device-width, initial-scale=1"/>
+            <link rel="icon" href="/favicon.ico"/>
+        </>
+    );
+}
--- nextjsblog-m/app/layout.tsx
+++ nextjsblog-m/app/layout.tsx
@@ -14,9 +14,30 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
+  const header = (
+    <header>
+      <div>
+        <h1>Augusto Leão Blog!</h1>
+        <p>Seja bem vindo ao meu blog!</p>
+      </div>
+    </header>
+  );
+
+  const footer = ( 
+    <footer>
+      <div>
+        <p>Augusto Leão Blog</p>
+      </div>
+    </footer>
+  )
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head />
+      {header}
+      <body>{children}</body> 
+      {footer}
     </html>
   )
 }
